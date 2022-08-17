6bb7d2c2644db2e6570dd8b613ae3cdffd0c3841
setren <abdullahfathurrahman96@gmail.com>
2022-08-17 08:05:22 +0700

    remove deprecated element marquee

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-import Footer from './components/footer';
-import Clock from './components/Clock';
+import logo from "./logo.svg";
+import "./App.css";
+import Footer from "./components/footer";
+import Clock from "./components/Clock";
 
 function App() {
- 
-const traceButton = () => {
-  alert("alhamdulillah");
-  console.log("button di click")
-};
-const paragraf = () => {
-    return(
+  const traceButton = () => {
+    alert("alhamdulillah");
+    console.log("button di click");
+  };
+  // eslint-disable-next-line
+  const paragraf = () => {
+    return (
       <div>
-    <h1>maaSyaaAllah</h1>
-    {<marquee><h1><Clock></Clock></h1></marquee> 
-    }
-    </div>
+        <h1>maaSyaaAllah</h1>
+        <span>
+          <h1>
+            <Clock></Clock>
+          </h1>
+        </span>
+      </div>
     );
-
-};
+  };
   return (
     <div className="App">
       <header className="App-header">
-      <marquee><h1><Clock></Clock></h1></marquee>
+        <span>
+          <h1>
+            <Clock></Clock>
+          </h1>
+        </span>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Belajar Progaming
-        </p>
+        <p>Belajar Progaming</p>
         <button onClick={() => traceButton()}>ubah navgasi</button>
-        <Footer paragraf={paragraf}/>
+        <Footer paragraf={paragraf} />
       </header>
     </div>
   );
